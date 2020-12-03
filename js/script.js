f516184403ad734df25ef3c2c648ad02bf8308ff
KushdeepSingh <skushdeep5@gmail.com>
2020-12-04 03:16:25 +0530

    completed for the first time ever

--- js/script.js
+++ js/script.js
@@ -9,22 +9,24 @@ var waypoint = new Waypoint({
 
 var j = 0;
 var waypoint = new Waypoint({
-  element: document.getElementById('hero'),
-  handler: function (direction) {
-    if (j % 2 === 0) {
-      document.getElementById('nav').style.position = "fixed";
-      document.getElementById('nav').style.top = "15vh";
-      j = j + 1;
-      console.log(j)
-      console.log("Entered Sticky")
-    }
-    else {
-      document.getElementById('nav').style.position = "absolute";
-      j = j + 1;
-      console.log(j)
-      console.log("Entered Absolute")
-    }
-  }
+  	element: document.getElementById('hero'),
+	handler: function (direction) {
+		if (j % 2 === 0) {
+			document.getElementById('nav').style.position = "fixed";
+			document.getElementById('nav').style.top = "15vh";
+			document.getElementById('arrowUp').classList.remove('inactive');
+			j = j + 1;
+			//   console.log(j)
+			//   console.log("Entered Sticky")
+		}
+		else {
+			document.getElementById('nav').style.position = "absolute";
+			document.getElementById('arrowUp').classList.add('inactive');
+			j = j + 1;
+			//   console.log(j)
+			//   console.log("Entered Absolute")
+		}
+	}
 })
 var m = 0;
 var waypoint = new Waypoint({
@@ -34,15 +36,15 @@ var waypoint = new Waypoint({
             document.getElementById('myPic').style.display = "block";
             document.getElementById('home').style.justifyContent = "space-between";
             m = m + 1;
-            console.log(m)
-            console.log("Entered Sticky")
-        }
-        else {
+            // console.log(m)
+            // console.log("Entered Sticky")
+		}
+		else {
             document.getElementById('myPic').style.display = "none";
             document.getElementById('home').style.justifyContent = "flex-end";
             m = m + 1;
-            console.log(m)
-            console.log("Entered Absolute")
+            // console.log(m)
+            // console.log("Entered Absolute")
         }
     }
 })
